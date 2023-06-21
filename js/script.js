443114d135ceb2c36518e5d6d9e6d18f8ca24662
luigimariapascali <luigimariapascali@gmail.com>
2023-06-21 17:36:37 +0200

    corretta e modificata prima parte esercizio

--- js/script.js
+++ js/script.js
@@ -1,22 +1,39 @@
-const gridContainer = document.querySelector('.grid-container');
-const startButton = document.querySelector('.start-button');
-
-// Aggiungo un event listener al bottone di avvio:
-startButton.addEventListener('click', () => {
-  // Rimuovo eventuali celle esistenti
-  gridContainer.innerHTML = '';
-
-  // Genero le celle della griglia:
-  for (let i = 1; i <= 100; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('grid-cell');
-    cell.textContent = i;
-    gridContainer.appendChild(cell);
-
-    // Aggiungo un event listener a ciascuna cella:
-    cell.addEventListener('click', () => {
-      cell.style.backgroundColor = '#7392B7';
-      alert(`Cell ${i} clicked`);
-    });
-  }
-});
+function generateBoard() {
+    const board = document.getElementById('board');
+    board.innerHTML = '';
+  
+    for (let row = 1; row <= 10; row++) {
+      // Creazione di un elemento div per rappresentare una riga
+      const rowElem = document.createElement('div');
+      rowElem.className = 'row';
+  
+      for (let col = 1; col <= 10; col++) {
+        // Creazione di un elemento div per rappresentare una cella
+        const cell = document.createElement('div');
+        cell.className = 'col-1 cell';
+        
+        // Assegnazione del testo alla cella corrente basato sulla posizione nella griglia
+        cell.innerText = (row - 1) * 10 + col;
+  
+        // Aggiunta dell'evento di click alla cella
+        cell.addEventListener('click', handleClick);
+  
+        // Aggiunta della cella alla riga corrente
+        rowElem.appendChild(cell);
+        }
+  
+      // Aggiunta della riga al contenitore della griglia
+      board.appendChild(rowElem);
+    }
+}
+  
+  function handleClick(event) {
+    const cell = event.target;
+    
+    // Aggiunta della classe 'clicked' alla cella cliccata
+    cell.classList.add('clicked');
+    
+    // Stampa del testo della cella cliccata nella console
+    console.log('Cella cliccata:', cell.innerText);
+}
+  
